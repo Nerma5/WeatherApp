3d36ffd6b852138dc1d44daaebd5f1c5b829a852
Nerma5 <nermahh5@gmail.com>
2023-06-30 19:14:28 +0200

    added func

--- src/components/Desktop.jsx
+++ src/components/Desktop.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import React, { useState } from "react";
 import { useQuery } from "react-query";
 import { Button } from "@mantine/core";
-import { Loader } from "@mantine/core";
+// import { Loader } from "@mantine/core";
 
 const Weather = () => {
   const fetchApp = async () => {
@@ -26,12 +26,12 @@ const Weather = () => {
   const { data: weatherData, isLoading } = useQuery(
     ["weather", searchedCity],
     fetchApp
-  );
+  );    
 
   return (
     <div className="mainDiv">
       <div>
-        <h1>Weather in </h1>
+        <h1>Weather in</h1>
         <div>
           <input type="text" value={searchCity} onChange={exploreCity} />
           <Button
@@ -39,21 +39,22 @@ const Weather = () => {
             radius="xl"
             size="md"
             uppercase
-            handleSubmit={searchInputHandler}
-            handleSearch={exploreCity}
+            onClick={searchInputHandler}
+            // handleSearch={exploreCity}
           >
             Search
           </Button>
         </div>
 
         {isLoading ? (
-           <Loader color="green" size="lg" />
+        //   <Loader color="green" size="lg" />
+        <p>loading</p>
         ) : (
           <div className="card">
-            <h2>Current Weather in {weatherData?.location?.name}</h2>
-            <p>Temperature: {weatherData?.current?.temp_c}°C</p>
-            <p>Humidity: {weatherData?.current?.humidity}%</p>
-            <p>Weather: {weatherData?.current?.condition?.text}</p>
+            <h2>Current Weather in {weatherData.location?.name}</h2>
+            <p>Temperature: {weatherData.current.temp_c}°C</p>
+            <p>Humidity: {weatherData.current.humidity}%</p>
+            <p>Weather: {weatherData.current.condition?.text}</p>
           </div>
         )}
       </div>

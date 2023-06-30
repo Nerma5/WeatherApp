d34652acbab1b2bf5f58fc58bd441b3e6068fa7b
Nerma5 <nermahh5@gmail.com>
2023-06-30 16:22:48 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+
+
+const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+  <BrowserRouter>
     <App />
+  </BrowserRouter>
+  </QueryClientProvider>
   </React.StrictMode>
 );
 
@@ -15,3 +28,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
